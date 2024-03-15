d70f61621c5d8c01677188cc39f6b6b6ac70f462
Yana <yana102hanan@gmail.com>
2024-03-15 12:18:00 +0200

    Change the elGallery to a global var

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -1,7 +1,9 @@
 "use strict"
 
+let gElGallery
+
 function renderGallery() {
-    const elGallery = document.querySelector('.gallery-container')
-    elGallery.innerHTML = "<img src='meme-imgs/meme-imgs (square)/1.jpg'></img>"
-    elGallery.innerHTML += "<img src='meme-imgs/meme-imgs (square)/2.jpg'>"
+    gElGallery = document.querySelector('.gallery-container')
+    gElGallery.innerHTML = "<img src='meme-imgs/meme-imgs (square)/1.jpg'></img>"
+    gElGallery.innerHTML += "<img src='meme-imgs/meme-imgs (square)/2.jpg'>"
 }
