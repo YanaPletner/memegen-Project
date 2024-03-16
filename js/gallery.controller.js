f0b8029455d6af0c79d0ccbf3b48d573a297cfd7
Yana <yana102hanan@gmail.com>
2024-03-16 13:06:48 +0200

    Render two images in gallery

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -4,12 +4,13 @@ let gElGallery
 
 function renderGallery() {
     gElGallery = document.querySelector('.gallery-container')
-    gElGallery.innerHTML = "<img src='meme-imgs/meme-imgs (square)/1.jpg' class='id-1' onclick='onImgSelect(this)'>"
-    gElGallery.innerHTML += "<img src='meme-imgs/meme-imgs (square)/2.jpg' class='id-2' onclick='onImgSelect(this)'>"
+    gElGallery.innerHTML = "<img src='meme-imgs/meme-imgs (square)/1.jpg' class='id-1.jpg' onclick='onImgSelect(this)'>"
+    gElGallery.innerHTML += "<img src='meme-imgs/meme-imgs (square)/2.jpg' class='id-2.jpg' onclick='onImgSelect(this)'>"
 }
 
 function onImgSelect(elImg) {
     const imgId = elImg.classList.value
+
     setImg(imgId)
     renderMeme()
 }
